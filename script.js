13f6d6753aef5fffea8d7fcdd235d01ac80df735
Breztdev <129894065+Bretzddev@users.noreply.github.com>
2023-05-19 14:49:29 +0200

    PlayerName's

---
+++ script.js
@@ -0,0 +1,16 @@
+let nameJ1 = document.getElementById('name1')
+let nameJ2 = document.getElementById('name2')
+let nomJ1 = document.getElementById('nom1')
+let nomJ2 = document.getElementById('nom2')
+
+nameJ1.addEventListener('blur', () => {nomJ1.innerText = nameJ1.value})
+nameJ2.addEventListener('blur', () => {nomJ2.innerText = nameJ2.value})
+
+
+
+
+    
+
+
+
+
